f66fb55ae00b3520522d0d2d3f29d6e49318eb19
Ubuntu <ubuntu@ip-172-31-2-45.ec2.internal>
2026-03-10 01:25:39 +0000

    prueba

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ import { ToastService } from '../../../core/services/toast.service';
   imports: [RouterLink, RouterLinkActive, CommonModule],
   template: `
     <nav class="navbar">
-      <a class="nav-brand" routerLink="/">🎵 CHINOOK</a>
+      <a class="nav-brand" routerLink="/">🎵 CHINOooooooooooK</a>
       <div class="nav-links">
         <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact:true}">Store</a>
         @if (auth.isLoggedIn()) {
